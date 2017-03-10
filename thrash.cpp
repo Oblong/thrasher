2137dbeec139d7015ca759a7b637761b957737b2
Zachary Michaels <zmichaels@oblong.com>
2017-03-10 08:50:44 -0800

    Break some long lines

--- thrash.cpp
+++ thrash.cpp
@@ -36,12 +36,30 @@ namespace {
 
 int main(int argc, char **argv) {
   args::ArgumentParser arg_parser{"A texture memory thrasher"};
-  args::HelpFlag help_flag{arg_parser, "help", "Display this message", {"help"}};
-  args::ValueFlag<std::size_t> width_flag{arg_parser, "WIDTH", "The width of a screen", {'w', "width"}, 500};
-  args::ValueFlag<std::size_t> height_flag{arg_parser, "HEIGHT", "The height of a screen", {'h', "height"}, 500};
-  args::ValueFlag<std::size_t> num_screens_flag{arg_parser, "SCREEN_COUNT", "The number of screens", {'s', "screens"}, 1};
-  args::Flag vertical_flag{arg_parser, "vertical", "Screens are vertically aligned (defaults to horizontal)", {'v', "vertical"}};
-  args::Flag alloc_buffers_flag{arg_parser, "alloc_buffers", "Allocate a new source buffer for each mip upload", {'a', "alloc_buffers"}};
+  args::HelpFlag help_flag{
+    arg_parser, "help", "Display this message", {"help"}
+  };
+  args::ValueFlag<std::size_t> width_flag{
+    arg_parser, "WIDTH", "The width of a screen", {'w', "width"}, 500
+  };
+  args::ValueFlag<std::size_t> height_flag{
+    arg_parser, "HEIGHT", "The height of a screen", {'h', "height"}, 500
+  };
+  args::ValueFlag<std::size_t> num_screens_flag{
+    arg_parser, "COUNT", "The number of screens", {'s', "screens"}, 1
+  };
+  args::Flag vertical_flag{
+    arg_parser,
+    "vertical",
+    "Screens are vertically aligned (defaults to horizontal)",
+    {'v', "vertical"}
+  };
+  args::Flag alloc_buffers_flag{
+    arg_parser,
+    "alloc_buffers",
+    "Allocate a new source buffer for each mip upload",
+    {'a', "alloc_buffers"}
+  };
   try {
     arg_parser.ParseCLI(argc, argv);
   } catch (args::Help) {
